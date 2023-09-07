a5cc3f45e3cb2816732d8f1a7659a91ace20d0cd
bmunday3 <bwmunday@gmail.com>
2023-09-07 16:43:45 -0400

    Working version of nantucket streamlit app with yolov8 running on Modzy

---
+++ nantucket-multipage/utils/util.py
@@ -0,0 +1,100 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+# read COCO labels used for YOLOv8 model
+ROOT = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(ROOT, "names.json"), "r") as file:
+    labels = json.load(file)
+NAMES = [v for _,v in labels.items()]
+
+# performance metrics
+def update_performance_table(inf, non_inf):
+    inference_mean = inf[["Frame Read Time", "Inference Time", "Postprocess Time", "Total"]].mean(axis=0)
+    # print(inference_mean)
+    inference_mean = round(inference_mean*1000, 2)
+    no_inference_mean = non_inf[["Frame Read Time", "Inference Time", "Postprocess Time", "Total"]].mean(axis=0)
+    no_inference_mean = round(no_inference_mean*1000, 2)
+    summary = pd.DataFrame(pd.concat([inference_mean, no_inference_mean], axis=1, ignore_index=True))
+    summ_t = summary.transpose()
+    summ_t["Type"] = ["Inference", "No Inference"]
+    summ_t = summ_t[["Type", "Frame Read Time", "Inference Time", "Postprocess Time", "Total"]]
+    summ_t["FPS"] = 1000 / summ_t["Total"]
+    return summ_t
+
+# chart data
+def format_table(df):
+    # confidence df
+    filtered_df = df[["Timestamp", "Object", "Confidence Score"]]
+    confidence_df = filtered_df.pivot(columns="Object", values="Confidence Score")   
+    # counts df 
+    counts = filtered_df.groupby(["Timestamp", "Object"])["Confidence Score"].count()
+    counts_df = counts.reset_index()
+    counts_df = counts_df.rename(columns={"Confidence Score": "Counts"})
+    counts_df = counts_df.pivot(columns="Object", values="Counts")
+    return confidence_df, counts_df
+
+# update analysis statistics
+def update_analysis(df, session_state):
+    # create total column
+    df["Total"] = df.iloc[:, 3:6].sum(axis=1)
+    # split into inference & non-inference
+    no_inference = df[df["Object"] == "No overlay"]
+    inference = df[df["Object"] != "No overlay"]   
+    # transform inference data into digestible format
+    conf_data, counts_data = format_table(inference) 
+    print('')
+    # summarize inference and non-inference tables into summary statistics
+    performance = update_performance_table(inference, no_inference)
+    # update session state
+    print('counts:\n\n{}'.format(counts_data.shape))
+    session_state.conf_data = conf_data
+    session_state.counts_data = counts_data
+    session_state.performance = performance
+    # return conf_data, counts_data, performance
+
+# convert bounding boxes from xywh to xyxy format
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = np.copy(x)
+    y[..., 0] = x[..., 0] - x[..., 2] / 2  # top left x
+    y[..., 1] = x[..., 1] - x[..., 3] / 2  # top left y
+    y[..., 2] = x[..., 0] + x[..., 2] / 2  # bottom right x
+    y[..., 3] = x[..., 1] + x[..., 3] / 2  # bottom right y
+    return y
+
+# create grid
+def make_grid(rows,cols,gap="small"):
+    grid = [0]*rows
+    for i in range(rows):
+        with st.container():
+            grid[i] = st.columns(cols, gap=gap)
+    return grid
+
+# colors for postprocessing
+class Colors:
+    def __init__(self):
+        hexs = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
+                '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7',
+                '753156', "5A28AB", 'A2FF76', "CCD840", "B496B4", "98DD41", "3FAEBE", "C13D54", "124D93", "C2E99D",
+                "5232F7", '0DE276', "43CE20", "106EA9", "5588CD", "0CA65C", 'F99769', '0071C7', '866291', '147E51',
+                '8E0144', 'E4A501', '816B33', '9D6A54', '341227', '390193', 'E001A7', '81850A', '374F50', '27DD49',
+                '61E6A9', '9F2598', '11AAA0', '465C6A', '2AB1B4', '3BAF48', 'F1CF62', '0F466D', 'A8F891', '8F3FDF',
+                '953DF9', "3E8101", '2E39F6', 'FA1806', '6F6AEB', '0A1099', '8B8953', '6347F2', 'B7A588', '060C8E',
+                "F45CDF", '5C8B79', '5043F6', '39AEC0', '3982B0', 'AE7136', '442E58', '577167', "4CDFD4", '839508')
+        self.palette = [self.hex2rgb(f'#{c}') for c in hexs]
+        self.n = len(self.palette)
+
+    def __call__(self, i, bgr=False):
+        c = self.palette[int(i) % self.n]
+        return (c[2], c[1], c[0]) if bgr else c
+
+    @staticmethod
+    def hex2rgb(h):  # rgb order (PIL)
+        return tuple(int(h[1 + i:1 + i + 2], 16) for i in (0, 2, 4))
+
+
+colors = Colors()  # create instance for 'from utils.plots import colors'
+
--- nantucket-multipage/video-stream.py
+++ nantucket-multipage/video-stream.py
@@ -42,71 +42,31 @@ MODEL_VERSION = "1.0.0"
 
 # create app grid
 st.title("AI-Powered Smart City Dashboard")
-page_grid = make_grid(2,2,gap="medium")
+st.divider()
+page_grid = make_grid(1,2,gap="medium")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# BOTTOM LEFT QUADRANT: video filter configuration
+# TOP RIGHT QUADRANT: video filter configuration
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-page_grid[1][0].markdown("### Video Configuration")
-page_grid[1][0].selectbox(
+page_grid[0][1].markdown("### Video Configuration")
+page_grid[0][1].selectbox(
     "*Available Model Options*", 
     options=["YOLOv8 Object Detection", "YOLOS Tiny Object Detection", "DETR ResNet-50 Object Detection", "DETR ResNet-50 Object Detection"], index=0)
-confidence_threshold = page_grid[1][0].slider(
+page_grid[0][1].divider()
+confidence_threshold = page_grid[0][1].slider(
     "*Confidence threshold*", 0.0, 1.0, 0.15, 0.05, key="slider_value"
 )
-class_filters = page_grid[1][0].multiselect(
+page_grid[0][1].text(" ")
+class_filters = page_grid[0][1].multiselect(
     label = "*Select which class labels to view*",
     options = NAMES,
     default = ['person', 'bicycle', 'car', 'motorcycle', 'traffic light', 'stop sign', 'truck', 'dog', 'parking meter', 'fire hydrant', 'bench', 'bird'],
     key = "class_filters"
 )
 
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# TOP RIGHT QUADRANT: 1/2 of Analysis
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-page_grid[0][1].markdown("### Analysis")
-page_grid[0][1].markdown("##### Vehicle Counts")
-with page_grid[0][1]:
-    metrics_grid = make_grid(1,4)
-    # TODO: make these dynamic
-    metrics_grid[0][1].metric(label="*Today*", value=1567, delta="37%")
-    metrics_grid[0][2].metric(label="*Last Hour*", value=35, delta="-78%")
-page_grid[0][1].markdown("##### Predictions Charts")
-with page_grid[0][1]:
-    title_subgrid = make_grid(1, 3)
-    title_subgrid[0][1].markdown("*Class Prediction Counts over Time*")  
-counts_placeholder = page_grid[0][1].empty()
-with page_grid[0][1]:
-    title_subgrid = make_grid(1, 3)
-    title_subgrid[0][1].markdown("*Confidence Scores over Time*")    
-confidence_placeholder = page_grid[0][1].empty()
-    
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# BOTTOM RIGHT QUADRANT: 2/2 of Analysis
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-page_grid[1][1].markdown("### Latency & Performance Statistics")
-with page_grid[1][1]:
-    title_subgrid = make_grid(1, 3)
-    title_subgrid[0][1].markdown("*Mean Latencies (ms) & FPS*")    
-performance_placeholder = page_grid[1][1].empty()
-refresh_button = page_grid[1][1].button(label="Refresh Analysis Results", use_container_width=True)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ANALYSIS REFRESH: updates multiple parts of page if pressed
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-if refresh_button:
-    confidence_df, counts_df, performance_data = update_analysis(st.session_state["aggregate_df"])
-    # class aggregated counts
-    counts_placeholder.area_chart(counts_df, width=300, height=300)
-    # confidence score
-    confidence_placeholder.line_chart(confidence_df, width=300, height=300)
-    performance_placeholder.dataframe(performance_data, hide_index=True, use_container_width=True)
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# TOP LEFT QUADRANT: video feed
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# BOTTOM RIGHT QUADRANT: video feed
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 page_grid[0][0].markdown("### Video Feed")
 frame_placeholder = page_grid[0][0].empty() # same video size --> see how i can format this
 with page_grid[0][0]:    
