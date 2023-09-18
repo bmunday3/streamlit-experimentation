c793216bd7104a08f929dc21020ddd8d27ccc011
bmunday3 <bwmunday@gmail.com>
2023-09-18 14:34:05 -0400

    Modified stream function and fixed widget actions

--- webcam/pose-detection-app/app.py
+++ webcam/pose-detection-app/app.py
@@ -1,12 +1,12 @@
 
 import streamlit as st
-from streamer import stream_both, stream_hands, stream_poses
+from streamer import stream
 from modzy import EdgeClient
 
 st.set_page_config(
     page_title="Pose Detection",
     page_icon="imgs/modzy_badge_v4.png",
-    layout="wide",
+    # layout="wide",
     initial_sidebar_state="expanded"
 )
 
@@ -14,7 +14,9 @@ st.sidebar.title('Model Selection')
 model_selection = st.sidebar.radio(label="Select model for inference", options=["Hand Landmark Detection", "Pose Landmark Detection", "Both"], index=2)
 st.sidebar.caption("*Note: Selecting 'Both' may reduce output video stream speed.*")
     
-client = EdgeClient('localhost', 55000) # change to IP address
+client = EdgeClient('localhost', 55000)
+# client = EdgeClient('10.0.0.240', 55000) # change to IP address
+# client = EdgeClient('host.docker.internal', 55000) # change to IP address
 client.connect()
 
 MODEL_MAPPING = {
@@ -28,12 +30,6 @@ MODEL_MAPPING = {
     }
 }
 
-print(model_selection)
-if model_selection == "Both":
-    stream_both(client, MODEL_MAPPING)
-elif model_selection == "Hand Landmark Detection":
-    stream_hands(client, MODEL_MAPPING)
-else:
-    stream_poses(client, MODEL_MAPPING)
+stream(model_selection, client, MODEL_MAPPING)
 
 
--- webcam/pose-detection-app/streamer.py
+++ webcam/pose-detection-app/streamer.py
@@ -16,79 +16,10 @@ mp_drawing_styles = mp.solutions.drawing_styles
 hands_connections = mp.solutions.hands.HAND_CONNECTIONS
 pose_connections = mp.solutions.pose.POSE_CONNECTIONS
 
-
-def stream_both(client, MODEL_MAPPING):
-    def process_both(image):
-        image.flags.writeable = False
-        # cv2.imwrite("multi-people.jpg", image)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        _, encoded_frame = cv2.imencode('.jpg', image)
-        input_object = InputSource(
-            key="image",
-            data=encoded_frame.tobytes()
-        )
-        start = time.time()
-        inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode    
-        inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
-        end = time.time()
-        print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")
-            
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # Draw the hand annotations on the image.
-        predictions_hands = [] if isinstance(inference_hands, list) else json.loads(inference_hands.result.outputs["results.json"].data)
-        if predictions_hands:
-            for landmarks in predictions_hands:
-                results_hands = landmark_pb2.NormalizedLandmarkList()
-                results_hands.ParseFromString(base64.b64decode(landmarks))
-                # results_hands.append(proto_object)       
-                # for hand_landmarks in results_hands2.landmark:
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=results_hands,
-                    connections=hands_connections,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
-                    connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style()
-                )
-            
-        # Draw the pose annotations on the image.     
-        predictions_poses = [] if isinstance(inference_poses, list) else json.loads(inference_poses.result.outputs["results.json"].data)           
-        if predictions_poses:
-            for landmarks in predictions_poses:
-                results_poses = landmark_pb2.NormalizedLandmarkList()
-                results_poses.ParseFromString(base64.b64decode(landmarks))
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=results_poses,
-                    connections=pose_connections,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-                )                       
-                                            
-        return cv2.flip(image, 1)
-
-
-    # webrtc component
-    RTC_CONFIGURATION = RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-    )
-    class VideoProcessor:
-        def recv(self, frame):
-            img = frame.to_ndarray(format="bgr24")
-            img = process_both(img)
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-    webrtc_ctx = webrtc_streamer(
-        key="WYH",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={"video": True, "audio": False},
-        video_processor_factory=VideoProcessor,
-        async_processing=True,
-    )
+def stream(model_selection, client, MODEL_MAPPING):
     
-def stream_poses(client, MODEL_MAPPING):
-    def process_poses(image):
+    def video_frame_callback(image):
+        image = image.to_ndarray(format="bgr24")
         image.flags.writeable = False
         # cv2.imwrite("multi-people.jpg", image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -96,139 +27,41 @@ def stream_poses(client, MODEL_MAPPING):
         input_object = InputSource(
             key="image",
             data=encoded_frame.tobytes()
-        )
-        start = time.time()
-        inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
-        end = time.time()
-        print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")      
-            
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # Draw the pose annotations on the image.     
-        predictions_poses = [] if isinstance(inference_poses, list) else json.loads(inference_poses.result.outputs["results.json"].data)           
-        if predictions_poses:
-            for landmarks in predictions_poses:
-                results_poses = landmark_pb2.NormalizedLandmarkList()
-                results_poses.ParseFromString(base64.b64decode(landmarks))
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=results_poses,
-                    connections=pose_connections,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-                )                       
-                                            
-        return cv2.flip(image, 1)
-
-
-    # webrtc component
-    RTC_CONFIGURATION = RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-    )
-    class VideoProcessor:
-        def recv(self, frame):
-            img = frame.to_ndarray(format="bgr24")
-            img = process_poses(img)
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-    webrtc_ctx = webrtc_streamer(
-        key="WYH",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={"video": True, "audio": False},
-        video_processor_factory=VideoProcessor,
-        async_processing=True,
-    )
-    
-def stream_hands(client, MODEL_MAPPING):
-    def process_hands(image):
-        image.flags.writeable = False
-        # cv2.imwrite("multi-people.jpg", image)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        _, encoded_frame = cv2.imencode('.jpg', image)
-        input_object = InputSource(
-            key="image",
-            data=encoded_frame.tobytes()
-        )
-
-        start = time.time()
-        inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode            
-        inference_poses = []
-        end = time.time()
-        print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms") 
-            
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # Draw the hand annotations on the image.
-        predictions_hands = [] if isinstance(inference_hands, list) else json.loads(inference_hands.result.outputs["results.json"].data)
-        if predictions_hands:
-            for landmarks in predictions_hands:
-                results_hands = landmark_pb2.NormalizedLandmarkList()
-                results_hands.ParseFromString(base64.b64decode(landmarks))
-                # results_hands.append(proto_object)       
-                # for hand_landmarks in results_hands2.landmark:
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=results_hands,
-                    connections=hands_connections,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
-                    connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style()
-                )              
-                                            
-        return cv2.flip(image, 1)
-
-
-    # webrtc component
-    RTC_CONFIGURATION = RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-    )
-    class VideoProcessor:
-        def recv(self, frame):
-            img = frame.to_ndarray(format="bgr24")
-            img = process_hands(img)
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-    webrtc_ctx = webrtc_streamer(
-        key="WYH",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={"video": True, "audio": False},
-        video_processor_factory=VideoProcessor,
-        async_processing=True,
-    )
-
-
-def stream_og(client, MODEL_MAPPING):
-
-    def process(image):
-        image.flags.writeable = False
-        # cv2.imwrite("multi-people.jpg", image)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        _, encoded_frame = cv2.imencode('.jpg', image)
-        input_object = InputSource(
-            key="image",
-            data=encoded_frame.tobytes()
-        )
-        print(model_selection)
+        )    
+        # print(model_selection, "in callback function")
         if model_selection == "Both":
+            # print('both!')
             start = time.time()
-            inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode    
-            inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
+            try:
+                inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode    
+                inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
+            except Exception as e:
+                print(e)
+                raise e
             end = time.time()
-            print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")
+            print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms") 
         elif model_selection == "Hand Landmark Detection":
+            # print('hands!')
             start = time.time()
-            inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode            
-            inference_poses = []
+            try:
+                inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode            
+                inference_poses = []
+            except Exception as e:
+                print(e)
+                raise e            
+            end = time.time()
+            print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")             
+        else:
+            # print("poses!")
+            start = time.time()
+            try:
+                inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
+                inference_hands = []
+            except Exception as e:
+                print(e)
+                raise e            
             end = time.time()
             print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")
-        else:
-            start = time.time()
-            inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
-            inference_hands = []
-            end = time.time()
-            print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")      
             
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -261,25 +94,19 @@ def stream_og(client, MODEL_MAPPING):
                     connections=pose_connections,
                     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
                 )                       
-                                            
-        return cv2.flip(image, 1)
-
-
+        image = cv2.flip(image, 1)                           
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+    
     # webrtc component
     RTC_CONFIGURATION = RTCConfiguration(
         {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
     )
-    class VideoProcessor:
-        def recv(self, frame):
-            img = frame.to_ndarray(format="bgr24")
-            img = process(img)
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
 
     webrtc_ctx = webrtc_streamer(
         key="WYH",
         mode=WebRtcMode.SENDRECV,
         rtc_configuration=RTC_CONFIGURATION,
+        video_frame_callback=video_frame_callback,
         media_stream_constraints={"video": True, "audio": False},
-        video_processor_factory=VideoProcessor,
         async_processing=True,
-    )
+    )                              
