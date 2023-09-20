5b2cf2a4a185ba188bc33322a7f94ecfa48005d4
bmunday3 <bwmunday@gmail.com>
2023-09-20 10:31:08 -0400

    Made updates to pose detection app

--- webcam/pose-detection-app/app.py
+++ webcam/pose-detection-app/app.py
@@ -6,13 +6,16 @@ from modzy import EdgeClient
 st.set_page_config(
     page_title="Pose Detection",
     page_icon="imgs/modzy_badge_v4.png",
-    # layout="wide",
+    layout="wide",
     initial_sidebar_state="expanded"
 )
 
 st.sidebar.title('Model Selection')
-model_selection = st.sidebar.radio(label="Select model for inference", options=["Hand Landmark Detection", "Pose Landmark Detection", "Both"], index=2)
-st.sidebar.caption("*Note: Selecting 'Both' may reduce output video stream speed.*")
+st.sidebar.markdown("Select model(s) for inference")
+faces = st.sidebar.toggle(label="Face Detection")
+hands = st.sidebar.toggle(label="Hand Landmark Detection")
+poses = st.sidebar.toggle(label="Pose Landmark Detection")
+st.sidebar.caption("*Note: Selecting more than one model at a time may reduce output video stream speed.*")
     
 client = EdgeClient('localhost', 55000)
 # client = EdgeClient('host.docker.internal', 55000) # change to IP address
@@ -26,9 +29,13 @@ MODEL_MAPPING = {
     "Pose Landmark Detection": {
         "id": "9odlhi0ylq",
         "version": "0.0.1"        
+    },
+    "Face Detection": {
+        "id": "r9zyziw6uk",
+        "version": "0.0.2"
     }
 }
 
-stream(model_selection, client, MODEL_MAPPING)
+stream({"faces": faces, "hands": hands, "poses": poses}, client, MODEL_MAPPING)
 
 
--- webcam/pose-detection-app/streamer.py
+++ webcam/pose-detection-app/streamer.py
@@ -6,7 +6,9 @@ import base64
 import numpy as np
 import av
 import mediapipe as mp
-from mediapipe.framework.formats import landmark_pb2
+from utils import visualize
+from mediapipe.framework.formats import landmark_pb2, detection_pb2
+from mediapipe.tasks.python.components.containers.detections import DetectionResult
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
 from modzy.edge import InputSource
@@ -21,79 +23,80 @@ def stream(model_selection, client, MODEL_MAPPING):
     def video_frame_callback(image):
         image = image.to_ndarray(format="bgr24")
         image.flags.writeable = False
-        # cv2.imwrite("multi-people.jpg", image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         _, encoded_frame = cv2.imencode('.jpg', image)
         input_object = InputSource(
             key="image",
             data=encoded_frame.tobytes()
-        )    
-        # print(model_selection, "in callback function")
-        if model_selection == "Both":
-            # print('both!')
-            start = time.time()
+        )
+        start = time.time()
+        count = 0
+        if model_selection["faces"]:
+            count += 1
             try:
-                inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode    
-                inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
+                inference_faces = client.inferences.perform_inference(MODEL_MAPPING["Face Detection"]["id"], MODEL_MAPPING["Face Detection"]["version"], [input_object]) # direct mode    
             except Exception as e:
                 print(e)
                 raise e
-            end = time.time()
-            print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms") 
-        elif model_selection == "Hand Landmark Detection":
-            # print('hands!')
-            start = time.time()
+            # Draw face bboxes on image
+            image.flags.writeable = True
+            predictions_faces = json.loads(inference_faces.result.outputs["results.json"].data)[0]
+            faces_proto = detection_pb2.DetectionList()
+            faces_proto.ParseFromString(base64.b64decode(predictions_faces))
+            detection_results = DetectionResult
+            detection_results = detection_results.create_from_pb2(faces_proto)
+            image = visualize(image, detection_results)            
+        
+        if model_selection["hands"]:
+            count += 1            
             try:
-                inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode            
-                inference_poses = []
+                inference_hands = client.inferences.perform_inference(MODEL_MAPPING["Hand Landmark Detection"]["id"], MODEL_MAPPING["Hand Landmark Detection"]["version"], [input_object]) # direct mode    
             except Exception as e:
                 print(e)
-                raise e            
-            end = time.time()
-            print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")             
-        else:
-            # print("poses!")
-            start = time.time()
+                raise e     
+            # Draw the hand annotations on the image.            
+            image.flags.writeable = True
+            predictions_hands = json.loads(inference_hands.result.outputs["results.json"].data)
+            if predictions_hands:
+                for landmarks in predictions_hands:
+                    results_hands = landmark_pb2.NormalizedLandmarkList()
+                    results_hands.ParseFromString(base64.b64decode(landmarks))
+                    # results_hands.append(proto_object)       
+                    # for hand_landmarks in results_hands2.landmark:
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=results_hands,
+                        connections=hands_connections,
+                        landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
+                        connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style()
+                    )
+            
+        if  model_selection["poses"]:
+            count += 1
             try:
                 inference_poses = client.inferences.perform_inference(MODEL_MAPPING["Pose Landmark Detection"]["id"], MODEL_MAPPING["Pose Landmark Detection"]["version"], [input_object]) # direct mode    
-                inference_hands = []
             except Exception as e:
                 print(e)
-                raise e            
-            end = time.time()
-            print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms")
+                raise e             
+            # Draw the pose annotations on the image.                 
+            image.flags.writeable = True
+            predictions_poses = json.loads(inference_poses.result.outputs["results.json"].data)           
+            if predictions_poses:
+                for landmarks in predictions_poses:
+                    results_poses = landmark_pb2.NormalizedLandmarkList()
+                    results_poses.ParseFromString(base64.b64decode(landmarks))
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=results_poses,
+                        connections=pose_connections,
+                        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
+                    )                                   
+     
+        end = time.time()
+        print(f"Processed frame in {round(((end-start)/60)*1000, 6)} ms with {count} models run")
             
         image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # Draw the hand annotations on the image.
-        predictions_hands = [] if isinstance(inference_hands, list) else json.loads(inference_hands.result.outputs["results.json"].data)
-        if predictions_hands:
-            for landmarks in predictions_hands:
-                results_hands = landmark_pb2.NormalizedLandmarkList()
-                results_hands.ParseFromString(base64.b64decode(landmarks))
-                # results_hands.append(proto_object)       
-                # for hand_landmarks in results_hands2.landmark:
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=results_hands,
-                    connections=hands_connections,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
-                    connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style()
-                )
-            
-        # Draw the pose annotations on the image.     
-        predictions_poses = [] if isinstance(inference_poses, list) else json.loads(inference_poses.result.outputs["results.json"].data)           
-        if predictions_poses:
-            for landmarks in predictions_poses:
-                results_poses = landmark_pb2.NormalizedLandmarkList()
-                results_poses.ParseFromString(base64.b64decode(landmarks))
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=results_poses,
-                    connections=pose_connections,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-                )                       
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)                      
         image = cv2.flip(image, 1)                           
         return av.VideoFrame.from_ndarray(image, format="bgr24")
     
